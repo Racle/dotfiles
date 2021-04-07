b84439f9d395d0654eb57e2a6ac770700b25e63a
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-04-07 14:54:47 +0300

    server settings

--- server/.config/nvim/lua/plug-config/bufferline-server.lua
+++ server/.config/nvim/lua/plug-config/bufferline-server.lua
@@ -1,5 +1,28 @@
 require "bufferline".setup {
+    highlights = {
+        fill = {
+            guibg = "#282828"
+        },
+        separator_selected = {
+            guifg = "#282828"
+        },
+        separator_visible = {
+            guifg = "#282828"
+        },
+        separator = {
+            guifg = "#282828"
+        }
+    },
     options = {
+        modified_icon = "●",
+        left_trunc_marker = "",
+        right_trunc_marker = "",
+        max_name_length = 25,
+        max_prefix_length = 25,
+        enforce_regular_tabs = false,
+        view = "multiwindow",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
         separator_style = "thick",
     }
 }
