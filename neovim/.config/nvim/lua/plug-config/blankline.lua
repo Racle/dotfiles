e0d66c7e23680c7262c2f11ca5ba67e4350c2553
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-04-11 01:29:50 +0300

    show first indent level

--- neovim/.config/nvim/lua/plug-config/blankline.lua
+++ neovim/.config/nvim/lua/plug-config/blankline.lua
@@ -9,4 +9,4 @@ vim.g.indent_blankline_filetype_exclude = {"help", "terminal"}
 vim.g.indent_blankline_buftype_exclude = {"terminal"}
 
 vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = false
+vim.g.indent_blankline_show_first_indent_level = true
