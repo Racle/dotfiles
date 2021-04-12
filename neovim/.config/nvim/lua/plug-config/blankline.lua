d6b1b6846484185cd73005a939b5d959f9952794
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-04-12 15:02:46 +0300

    disabled trailing indent

--- neovim/.config/nvim/lua/plug-config/blankline.lua
+++ neovim/.config/nvim/lua/plug-config/blankline.lua
@@ -2,8 +2,11 @@ local indent = 2
 
 vim.g.indentLine_enabled = 1
 vim.g.indent_blankline_char = "▏"
+vim.g.indent_blankline_show_trailing_blankline_indent = false
 
 vim.cmd("hi IndentBlanklineChar guifg=#373b43")
 
 vim.g.indent_blankline_filetype_exclude = {"help", "terminal"}
 vim.g.indent_blankline_buftype_exclude = {"terminal"}
+
+
