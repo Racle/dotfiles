1c32f7fa8980d26b547f9a35d202716519fa9c5a
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-04-12 08:42:43 +0300

    removed unused configs

--- neovim/.config/nvim/lua/plug-config/blankline.lua
+++ neovim/.config/nvim/lua/plug-config/blankline.lua
@@ -7,6 +7,3 @@ vim.cmd("hi IndentBlanklineChar guifg=#373b43")
 
 vim.g.indent_blankline_filetype_exclude = {"help", "terminal"}
 vim.g.indent_blankline_buftype_exclude = {"terminal"}
-
-vim.g.indent_blankline_show_trailing_blankline_indent = false
-vim.g.indent_blankline_show_first_indent_level = true
