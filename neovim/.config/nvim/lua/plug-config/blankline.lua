2c6fab46135db4552732d0450fc5111727c08a9c
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-04-27 08:14:23 +0300

    Merge

--- neovim/.config/nvim/lua/plug-config/blankline.lua
+++ neovim/.config/nvim/lua/plug-config/blankline.lua
@@ -1,6 +1,7 @@
 vim.g.indentLine_enabled = 1
 vim.g.indent_blankline_char = "▏"
 vim.g.indent_blankline_show_trailing_blankline_indent = false
+-- disabled now for performance hit.
 -- vim.g.indent_blankline_use_treesitter = true
 vim.g.indent_blankline_show_current_context = true
 
