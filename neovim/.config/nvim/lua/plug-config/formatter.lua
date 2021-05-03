a92d4209388d7917f7fbac789f5b8bd8b5a7dec5
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-05-03 09:06:23 +0300

    changed php formatting from formatter to prettier

--- neovim/.config/nvim/lua/plug-config/formatter.lua
+++ neovim/.config/nvim/lua/plug-config/formatter.lua
@@ -1,8 +1,9 @@
+-- autocmd BufWritePost *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.md,*.vue,*.yaml,*.html,*.xml,*.php,*.lua,*.rs,*.conf FormatWrite
 vim.api.nvim_exec(
   [[
 augroup FormatAutogroup
   autocmd!
-  autocmd BufWritePost *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.md,*.vue,*.yaml,*.html,*.xml,*.php,*.lua,*.rs,*.conf FormatWrite
+  autocmd BufWritePost *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.md,*.vue,*.yaml,*.html,*.xml,*.lua,*.rs,*.conf FormatWrite
 augroup END
 ]],
   true
@@ -36,7 +37,6 @@ require("formatter").setup(
       yaml = prettier,
       html = prettier,
       xml = prettier,
-      php = prettier,
       lua = {
         -- luafmt
         function()
--- neovim/.config/nvim/plug-config/prettier.vim
+++ neovim/.config/nvim/plug-config/prettier.vim
@@ -3,3 +3,4 @@
 let g:prettier#autoformat_require_pragma = 0
 let g:prettier#autoformat = 0
 " au BufWritePre *.js,*.jsx,*.mjs,*.ts,*.tsx,*.css,*.less,*.scss,*.json,*.graphql,*.md,*.vue,*.yaml,*.html,*.xml,*.php PrettierAsync
+au BufWritePre *.php PrettierAsync
