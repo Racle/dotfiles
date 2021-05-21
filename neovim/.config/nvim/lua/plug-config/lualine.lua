100e969fa57ccef0fcc2b39da7efb06cd9988be8
Ville Viitaharju <ville.viitaharju@festum.fi>
2021-05-21 15:13:19 +0300

    updated lualine config

--- neovim/.config/nvim/lua/plug-config/lualine.lua
+++ neovim/.config/nvim/lua/plug-config/lualine.lua
@@ -1,7 +1,11 @@
 require "lualine".setup {
   options = {theme = "gruvbox"},
   sections = {
+    lualine_a = {"mode"},
     lualine_b = {"branch", "diff"},
-    lualine_c = {{"diagnostics", sources = {"coc"}}, "filename", "b:coc_current_function"}
+    lualine_c = {{"diagnostics", sources = {"coc"}}, "filename", "b:coc_current_function"},
+    lualine_x = {"encoding", "fileformat"},
+    lualine_y = {"filetype"},
+    lualine_z = {"location"}
   }
 }
