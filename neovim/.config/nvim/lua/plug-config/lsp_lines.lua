b8970ff00a7e6bf7a3b5a0a2a4db92fbd1326ea4
Ville Viitaharju <ville.viitaharju@visma.com>
2023-01-23 13:32:02 +0200

    500ms delay to see lsp_lines on current line

--- neovim/.config/nvim/lua/plug-config/lsp_lines.lua
+++ neovim/.config/nvim/lua/plug-config/lsp_lines.lua
@@ -1,2 +1,35 @@
 -- for lsp_lines, show only when hovering
-vim.cmd [[autocmd! CursorHold * lua vim.diagnostic.config({ virtual_lines = { only_current_line = true } })]]
+-- vim.cmd [[autocmd! CursorHold * lua vim.diagnostic.config({ virtual_lines = { only_current_line = true } })]]
+-- local AAA
+vim.diagnostic.config({virtual_lines = false})
+
+-- vim.cmd [[autocmd! CursorHold * lua vim.diagnostic.config({ virtual_lines = { only_current_line = true } })]]
+
+local timer = vim.loop.new_timer()
+
+vim.api.nvim_create_autocmd(
+  "CursorHold",
+  {
+    callback = function()
+      vim.diagnostic.config({virtual_lines = false})
+      timer:start(
+        500,
+        0,
+        vim.schedule_wrap(
+          function()
+            vim.diagnostic.config({virtual_lines = {only_current_line = true}})
+          end
+        )
+      )
+    end
+  }
+)
+vim.api.nvim_create_autocmd(
+  "CursorMoved",
+  {
+    callback = function()
+      timer:stop()
+      vim.diagnostic.config({virtual_lines = false})
+    end
+  }
+)
