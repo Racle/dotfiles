234b5f1c4d8e4b1095bdabc5317ca79c40e38884
Ville Viitaharju <ville.viitaharju@visma.com>
2023-01-25 15:46:51 +0200

    changed neoscroll time from 0 to 1

--- neovim/.config/nvim/lua/plug-config/neoscroll.lua
+++ neovim/.config/nvim/lua/plug-config/neoscroll.lua
@@ -2,10 +2,10 @@ require("neoscroll").setup({})
 
 local t = {}
 -- Syntax: t[keys] = {function, {function arguments}}
-t["<C-u>"] = {"scroll", {"-vim.wo.scroll", "true", "0"}}
-t["<C-d>"] = {"scroll", {"vim.wo.scroll", "true", "0"}}
-t["<C-b>"] = {"scroll", {"-vim.api.nvim_win_get_height(0)", "true", "0"}}
-t["<C-f>"] = {"scroll", {"vim.api.nvim_win_get_height(0)", "true", "0"}}
+t["<C-u>"] = {"scroll", {"-vim.wo.scroll", "true", "1"}}
+t["<C-d>"] = {"scroll", {"vim.wo.scroll", "true", "1"}}
+t["<C-b>"] = {"scroll", {"-vim.api.nvim_win_get_height(0)", "true", "1"}}
+t["<C-f>"] = {"scroll", {"vim.api.nvim_win_get_height(0)", "true", "1"}}
 t["<C-y>"] = {"scroll", {"-0.10", "false", "100"}}
 t["<C-e>"] = {"scroll", {"0.10", "false", "100"}}
 t["zt"] = {"zt", {"100"}}
