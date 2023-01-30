e6bb4e550e10d71a2c4d1d2cf5ec33ad9613d64c
Ville Viitaharju <ville.viitaharju@visma.com>
2023-01-30 16:58:00 +0200

    changed lightspeed to leap

--- neovim/.config/nvim/lua/general/plugins.lua
+++ neovim/.config/nvim/lua/general/plugins.lua
@@ -252,7 +252,28 @@ return require("packer").startup(
     -- % matches to functions etc
     use "andymass/vim-matchup"
     -- Quick jumping around
-    use "ggandor/lightspeed.nvim"
+    use {
+      "ggandor/leap.nvim",
+      config = function()
+        require("leap").add_default_mappings()
+        -- The below settings make Leap's highlighting a bit closer to what you've been
+        -- used to in Lightspeed.
+        vim.api.nvim_set_hl(0, "LeapBackdrop", {link = "Comment"})
+        vim.api.nvim_set_hl(
+          0,
+          "LeapMatch",
+          {
+            fg = "white", -- for light themes, set to 'black' or similar
+            bold = true,
+            nocombine = true
+          }
+        )
+        -- keep old 'x' behaviour in visual mode
+        vim.keymap.del({"x", "o"}, "x")
+        vim.keymap.del({"x", "o"}, "X")
+        require("leap").opts.highlight_unlabeled_phase_one_targets = true
+      end
+    }
     -- smooth scrolling
     use "karb94/neoscroll.nvim"
     -- vscode like minimap/codewindow
