26dd21722d5041f13aa4a3e2b59e658da4844df8
Ville Viitaharju <ville.viitaharju@visma.com>
2023-08-07 10:18:06 +0300

    removed unused gitsigns mappings

--- neovim/.config/nvim/lua/plug-config/gitsigns.lua
+++ neovim/.config/nvim/lua/plug-config/gitsigns.lua
@@ -1,12 +1,12 @@
-local gitsigns = require'gitsigns'
+local gitsigns = require "gitsigns"
 
 vim.o.termguicolors = true
 
 gitsigns.setup {
-  keymaps = {
-    buffer = true,
-    ['n ]c'] = '<cmd>lua require"gitsigns".next_hunk({wrap = true})<CR>',
-    ['n [c'] = '<cmd>lua require"gitsigns".prev_hunk({wrap = true})<CR>',
-  },
+  -- keymaps = {
+  --   buffer = true,
+  --   ['n ]c'] = '<cmd>lua require"gitsigns".next_hunk({wrap = true})<CR>',
+  --   ['n [c'] = '<cmd>lua require"gitsigns".prev_hunk({wrap = true})<CR>',
+  -- },
   max_file_length = 100000
 }
