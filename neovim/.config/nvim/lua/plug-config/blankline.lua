2ef5a6af9003da27bfb81188805e11d1a852662f
Ville Viitaharju <ville.viitaharju@visma.com>
2023-11-08 07:57:49 +0200

    updated config

--- neovim/.config/nvim/lua/plug-config/blankline.lua
+++ neovim/.config/nvim/lua/plug-config/blankline.lua
@@ -1,24 +1,15 @@
-require("indent_blankline").setup {
-  char = "▏",
-  buftype_exclude = {"terminal"},
-  show_trailing_blankline_indent = false,
-  show_current_context = true,
-  filetype_exclude = {"help", "terminal"},
-  -- default : {'class', 'function', 'method'}
-  context_patterns = {
-    "class",
-    "function",
-    "method",
-    "^if",
-    "^while",
-    "^for",
-    "^object",
-    "^table",
-    "^type",
-    "^import",
-    "block",
-    "arguments"
+require("ibl").setup {
+  indent = {
+    char = "▏"
   },
-  -- disabled now for performance hit.
-  use_treesitter = true
+  exclude = {
+    buftypes = {"terminal"},
+    filetypes = {"help", "packer"}
+  },
+  scope = {
+    enabled = true,
+    show_start = false,
+    highlight = {"Label"},
+    char = "▏"
+  }
 }
