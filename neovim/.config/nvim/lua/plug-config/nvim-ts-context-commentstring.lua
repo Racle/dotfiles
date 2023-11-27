f22c2008e59aeb6d935f6181143c204d47baa19e
Ville Viitaharju <ville.viitaharju@visma.com>
2023-11-27 15:15:55 +0200

    updated nvim-ts-context-commentstring config to latest

--- neovim/.config/nvim/lua/plug-config/nvim-ts-context-commentstring.lua
+++ neovim/.config/nvim/lua/plug-config/nvim-ts-context-commentstring.lua
@@ -1,5 +1,2 @@
-require'nvim-treesitter.configs'.setup {
-  context_commentstring = {
-    enable = true
-  }
-}
+require("ts_context_commentstring").setup {}
+vim.g.skip_ts_context_commentstring_module = true
