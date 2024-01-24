65c518895851094cd0b34e15319c3c99f76b5fa9
Ville Viitaharju <ville.viitaharju@visma.com>
2024-01-24 12:26:15 +0200

    neoscroll performance boost

--- neovim/.config/nvim/lua/plug-config/neoscroll.lua
+++ neovim/.config/nvim/lua/plug-config/neoscroll.lua
@@ -1,11 +1,31 @@
-require("neoscroll").setup({})
+-- require("neoscroll").setup({})
+require("neoscroll").setup(
+  {
+    pre_hook = function()
+      vim.opt.eventignore:append(
+        {
+          "WinScrolled",
+          "CursorMoved"
+        }
+      )
+    end,
+    post_hook = function()
+      vim.opt.eventignore:remove(
+        {
+          "WinScrolled",
+          "CursorMoved"
+        }
+      )
+    end
+  }
+)
 
 local t = {}
 -- Syntax: t[keys] = {function, {function arguments}}
 t["<C-u>"] = {"scroll", {"-vim.wo.scroll", "true", "1"}}
 t["<C-d>"] = {"scroll", {"vim.wo.scroll", "true", "1"}}
-t["<C-b>"] = {"scroll", {"-vim.api.nvim_win_get_height(0)", "true", "1"}}
-t["<C-f>"] = {"scroll", {"vim.api.nvim_win_get_height(0)", "true", "1"}}
+t["<C-b>"] = {"scroll", {"-vim.fn.winheight(0)", "true", "1"}}
+t["<C-f>"] = {"scroll", {"vim.fn.winheight(0)", "true", "1"}}
 t["<C-y>"] = {"scroll", {"-0.10", "false", "100"}}
 t["<C-e>"] = {"scroll", {"0.10", "false", "100"}}
 t["zt"] = {"zt", {"100"}}
